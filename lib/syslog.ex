fb7c32845a902e86594b98cbf0af67e562f18c43
Stian Grytøyr <stian@grytoyr.net>
2018-03-24 20:16:01 +0100

    Testing fix for DataDog Logs

--- lib/syslog.ex
+++ lib/syslog.ex
@@ -57,7 +57,8 @@ defmodule Logger.Backends.Syslog do
     level_num = Logger.Syslog.Utils.level(level)
     pre = :io_lib.format('<~B>~s ~s~p: ', [facility ||| level_num,
       Logger.Syslog.Utils.iso8601_timestamp(ts), appid, self()])
-    packet = [pre, Logger.Formatter.format(format, level, msg, ts, Keyword.take(md, metadata))]
+    # packet = [pre, Logger.Formatter.format(format, level, msg, ts, Keyword.take(md, metadata))]
+    packet = [Logger.Formatter.format(format, level, msg, ts, Keyword.take(md, metadata))]
     IO.puts(inspect packet)
     if socket, do: :gen_udp.send(socket, host, port, packet)
   end
